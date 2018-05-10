cf6ba51a3ae2ca20d66568ab0f10150ee0b1adf0
afarangurens <afarangurens@unal.edu.co>
2018-05-09 23:30:08 -0400

    Automatic commit. Done before go to queltrics

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
-    });
-})();
+function initializeMap(){
+    var mapDiv = document.getElementById('map');
+    
+    var map = new google.maps.Map(mapDiv, {
+        center: {lat: 40.7290379, lng: -73.9965486},
+        zoom: 12}); 
+        
+    var marker = new google.maps.Marker({ //Line 1
+        position: {lat: 40.7290379, lng: -73.9965486}, 
+        map: map})
+       
+}
+
